2328b8ff1d6ac1921ee59c02028ad061bcdbc719
*mrds994* <*bagas345b@gmail.com*>
2023-03-24 06:37:51 +0700

    tes

---
+++ baseikal/command/allmenu/allmenu.js
@@ -0,0 +1,29 @@
+const allmenu =  ` 
+*Random*
+┏━━⊱
+┣❏kalkulator
+┣❏toimage
+┣❏toonce
+┣❏tomp4
+┣❏tovn
+┣❏togif
+┣❏tourl 
+┣❏toaud
+┣❏sticker
+┣❏stickergif
+┣❏emojimix
+┣❏emojimix2
+┣❏smeme
+┣❏ktpmaker
+┣❏afk
+┣❏* [ reply ]
+┣❏getname [ reply ]
+┣❏getpic [ reply ]
+┣❏infochat
+┣❏q [ reply ]
+┣❏del [ reply ]
+┣❏style [ text nya ]
+┣❏ss [link nya]
+┣❏penjara [ reply ]
+┗━━⊱[ MrDs ]`
+exports.allmenu = allmenu
---
+++ baseikal/command/menu/menu.js
@@ -0,0 +1,36 @@
+const menu =  ` 
+° Owner  : MrDs
+° Version : 15
+° Baileys : 5.0.0
+
+*Menu Fun*
+┏━━⊱
+┣❏ Jadian
+┣❏ Artinama
+┣❏ Artimimpi
+┣❏ Kecocokanpasangan
+┣❏ Kecocokannama
+┣❏ Jadianpernikahan
+┣❏ Rejeki
+┣❏ Sifatusaha
+┣❏ Pekerjaan
+┣❏ Artitarot
+┣❏ Potensipenyakit
+┣❏ Ramalannasib
+┣❏ Harisangar
+┣❏ Haribaik
+┣❏ Fengshui
+┣❏ Nagahari
+┣❏ Harinaas
+┣❏ Weton
+┣❏ Peruntungan
+┣❏ Arahrejeki
+┣❏ Sifat
+┣❏ Keberuntungan
+┣❏ Memancing
+┣❏ Masasubur
+┣❏ Zodiak
+┣❏ Shio
+┗━━⊱
+ ▰▱▰▱▰▱▰▱▰▱▰▱▰▱`
+exports.menu = menu
